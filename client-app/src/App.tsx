a220aed6ddad64fd2bbbe9c4756bc90d66b9f899
Dedeaga Delia <delia.dedeaga@kantar.com>
2021-05-22 18:23:09 +0300

    Second commit - client part1

---
+++ client-app/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/books").then((response) => {
+      console.log(response);
+      setBooks(response.data);
+    });
+  }, []);
+
+  return (
+    <div >
+      <Header as="h2" icon="book" content="Books" />
+      <List>
+        {books.map((book: any) => (
+          <List.Item key={book.id}>{book.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
